8cf69879626692f6e39b9350c6df68da0aedc8b3
Onur Osmanov <66003294+Pand161@users.noreply.github.com>
2023-08-08 13:18:41 +0300

    added routing and shared folder
    added routing and shared folder

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './features/auth/auth.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CoreModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project/src/app/features/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { AuthRoutingModule } from "./auth-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../../shared/shared.module";
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    SharedModule,
+  ],
+})
+export class AuthModule {}
